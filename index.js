7174cf1fc7dca17dbbbd48fdcbd1d028cc77149d
Shaopeng Zhang <szhangpitt@gmail.com>
2017-02-12 23:58:50 -0500

    awesome

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-import Rx from 'rxjs/Rx';
 import run from './utils/run';
-
-import reportTableView from './components/report-table';
+import reportTableView, {update} from './components/report-table';
 import reportData from './data/report';
 
 const target = document.getElementById('target');
-const stream = Rx.Observable
-.of({report: reportData, selectedRowIndex: 1});
-
-const handler = {
-    rowclick: (item, index) => {
-        console.log(`row clicked: ${JSON.stringify(item)}, ${index}`);
-    },
-    colclick: (label, index) => {
-        console.log(`col clicked: ${label}, ${index}`);
-    },
+const initState = {
+    report: reportData,
+    selectedRowIndex: -1,
+    selectedColIndex: -1,
 };
 
-const observable = new Rx.Observable(obs =>
-    run(stream, reportTableView(handler, obs), target)
+const {allState} = run(
+    initState,
+    update,
+    reportTableView,
+    target
 );
 
-observable.subscribe(console.warn.bind(console));
+allState
+.subscribe(as => {
+    console.log('all state', as);
+})
--- utils/run.js
+++ utils/run.js
@@ -1,7 +1,29 @@
 import patch from './patch';
+import Rx from 'rxjs/Rx';
 
-export default function run (stream, view, target) {
-    const subscriber = stream
+function makeDispatch (initState, update) {
+    const bsub = new Rx.BehaviorSubject(initState);
+
+    return {
+        allState: bsub,
+        dispatch (action) {
+            const newState = update(bsub.getValue(), action);
+            bsub.next(newState);
+        },
+    };
+}
+
+// initState ---i----------
+// newState  -------u---u--
+// vnode     ---v---v---v--
+// patch     ---p---p---p--
+
+export default function run (initState, update, viewFn, target) {
+    const {allState, dispatch} = makeDispatch(initState, update);
+    const view = viewFn(dispatch);
+
+    const subscriber =
+    allState
     .map(view)
     .startWith(target)
     .pairwise()
@@ -10,7 +32,11 @@ export default function run (stream, view, target) {
         patch(prevVnode, curVnode)
     );
 
-    return function un () {
-        subscriber.unsubscribe();
+    return {
+        unsubscribe () {
+            subscriber.unsubscribe();
+        },
+        allState: allState,
     };
 }
+
